d5f626805f82a7fca0aa2dc669fbb1a4b044b4dd
vamsi113 <krishultimate5@gmail.com>
2023-05-10 12:13:34 -0400

    updating roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,3 +3,32 @@ def codeQuality() {
         echo 'Code Quality'
     }
 }
+
+def codeChecks() {
+    if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*") {
+        stage('Style Checks') {
+            echo 'Style Checks'
+        }
+        stage('Unit Tests') {
+            echo 'Unit Tests'
+        }
+
+    }
+}
+
+def artifacts() {
+    if ( TAG_NAME ==~ ".*") {
+        stage('Download Dependancies') {
+            echo 'Download Dependancies'
+        }
+        stage('Prepare Artifacts') {
+            echo 'Prepare Artifacts'
+        }
+        stage('Publish Artifacts') {
+            echo 'Publish Artifacts'
+        }
+
+    }
+}
+
+
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,5 +1,4 @@
 def call() {
-
     node {
         if (! env.TAG_NAME) {
             env.TAG_NAME = ""
@@ -7,64 +6,8 @@ def call() {
 
         sh 'env'
         common.codeQuality()
-
-        if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*") {
-            stage('Style Checks') {
-                echo 'Style Checks'
-            }
-        }
-
+        common.codeChecks()
+        common.artifacts()
 
     }
-
-
-
-//    pipeline {
-//        agent any
-//        stages {
-//
-
-//
-
-//                when {
-//                    anyOf{
-//                        branch "main"
-//                        tag "*"
-//                    }
-//                }
-//                steps {
-//                    echo 'Style Checks'
-//                }
-//            }
-//            stage('Unit Tests') {
-//                when {
-//                    anyOf{
-//                        branch "main"
-//                        tag "*"
-//                    }
-//                }
-//                steps {
-//                    echo 'Unit Tests'
-//                }
-//            }
-//            stage('Download Dependancies') {
-//                when { tag "*"}
-//                steps {
-//                    echo 'Download Dependancies'
-//                }
-//            }
-//            stage('Prepare Artifacts') {
-//                when { tag "*"}
-//                steps {
-//                    echo 'Prepare Artifacts'
-//                }
-//            }
-//            stage('Publish Artifacts') {
-//                when { tag "*"}
-//                steps {
-//                    echo 'Publish Artifacts'
-//                }
-//            }
-//        }
-//    }
 }
