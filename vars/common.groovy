d4cd62455f335e2756a6092fef325ee79e0a4800
vamsi113 <krishultimate5@gmail.com>
2023-05-10 12:17:09 -0400

    chang

--- vars/common.groovy
+++ vars/common.groovy
@@ -5,7 +5,7 @@ def codeQuality() {
 }
 
 def codeChecks() {
-    if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*") {
+    if ( env.BRANCH_NAME == "main" || env.TAG_NAME ==~ ".*") {
         stage('Style Checks') {
             echo 'Style Checks'
         }
@@ -17,7 +17,7 @@ def codeChecks() {
 }
 
 def artifacts() {
-    if ( TAG_NAME ==~ ".*") {
+    if ( env.TAG_NAME ==~ ".*") {
         stage('Download Dependancies') {
             echo 'Download Dependancies'
         }
