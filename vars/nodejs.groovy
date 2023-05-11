0f065e403993e5df79419ace9d59138b9d186e74
vamsi113 <krishultimate5@gmail.com>
2023-05-10 21:46:10 -0400

    updating roboshop-jenkins-shared-library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,10 +1,15 @@
 def call() {
     node {
+        try {
+            common.codeQuality()
+            common.codeChecks()
+            common.artifacts()
+
+        } catch (Exception e) {
+            mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'sudddapalli@gmail.com', replyTo: '', subject: 'BUILD_FAILURE', to: 'sudddapalli@gmail.com'
+        }
 
         sh 'env'
-        common.codeQuality()
-        common.codeChecks()
-        common.artifacts()
 
     }
 }
