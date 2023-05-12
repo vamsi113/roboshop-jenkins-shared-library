28301fcd14dd6162b1a8927f5f6485801fbb7f7b
vamsi113 <krishultimate5@gmail.com>
2023-05-12 10:08:16 -0400

    updating roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,7 +3,7 @@ def codeQuality() {
         withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'sonarPass', usernameVariable: 'sonarUser')]) {
             sh '''
             sonar-scanner -Dsonar.host.url=http://172.31.15.159:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true 
-       '''
+            '''
         }
     }
 }
