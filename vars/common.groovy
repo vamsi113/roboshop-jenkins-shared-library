ea793127eff08a29060fd3076b494cbbbf6dfafc
vamsi113 <krishultimate5@gmail.com>
2023-05-12 16:26:24 -0400

    chang

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,6 +1,6 @@
 def codeCheckout() {
     stage('code Checkout') {
-        sh 'find . | sed 1d |xargs rm -f'
+        sh 'find . | sed 1d |xargs rm -rf'
         git branch: 'main', url: "https://github.com/vamsi113/${COMPONENT}.git"
     }
 }
