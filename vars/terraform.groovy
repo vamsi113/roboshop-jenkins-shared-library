ab6bb7194aff28b730bedeaa4b0f7d0e9587c0f9
vamsi113 <krishultimate5@gmail.com>
2023-06-24 11:58:50 -0400

    change

---
+++ vars/terraform.groovy
@@ -0,0 +1,31 @@
+def call() {
+    pipeline {
+        agent any
+
+        options {
+            ansiColor('xterm')
+        }
+
+        parameters {
+
+
+            choice(name: 'ENV', choices: ['', 'dev', 'prod'], description: 'environment')
+            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'action')
+        }
+
+        stages {
+
+            stage('terraform') {
+
+                steps {
+                    sh  '''
+                        rm -rf .terraform*
+                        terrafile -f env-${ENV}/Terrafile
+                        terraform init -backend-config=env-${ENV}/backend.tfvars
+                        terraform ${ACTION} -auto-approve -var-file=env-${ENV}/main.tfvars
+                    '''
+                }
+            }
+        }
+    }
+}
