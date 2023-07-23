c35a2c3e7091057101c11376ef4b511deef3ccff
vamsi113 <krishultimate5@gmail.com>
2023-07-23 18:05:20 -0400

    change

--- vars/common.groovy
+++ vars/common.groovy
@@ -6,16 +6,16 @@ def codeCheckout() {
 }
 
 
-def codeQuality() {
-    stage('Code Quality') {
-        withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'sonarPass', usernameVariable: 'sonarUser')]) {
-            sh '''
-               #sonar-scanner -Dsonar.host.url=http://172.31.15.159:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}
-               echo OK
-            '''
-        }
-    }
-}
+//def codeQuality() {
+//    stage('Code Quality') {
+//        withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'sonarPass', usernameVariable: 'sonarUser')]) {
+//            sh '''
+//               sonar-scanner -Dsonar.host.url=http://172.31.15.159:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}
+//               echo OK
+//            '''
+//        }
+//    }
+//}
 
 def codeChecks() {
     if ( env.BRANCH_NAME == "main" || env.TAG_NAME ==~ ".*") {
